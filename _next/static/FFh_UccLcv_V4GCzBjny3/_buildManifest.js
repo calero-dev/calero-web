e3cf16ea5dfeb14991a9049b76d44cd11ca6a28e
Raúl Gómez Calero <shenrgc@gmail.com>
2023-12-11 09:53:53 +0100

    Updates

---
+++ _next/static/FFh_UccLcv_V4GCzBjny3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/268-368bd713d92bd02f.js","static/chunks/pages/index-23fc9258edb67d67.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
